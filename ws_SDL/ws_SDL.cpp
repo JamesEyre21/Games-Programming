ed974c04fefd19862fd81b6fa34a8d49a2b29fdc
JamesEyre21 <james.o.eyre@gmail.com>
2024-02-18 20:07:27 +0000

    Creates a window with a dark background and draws a red rectangle.

--- ws_SDL/ws_SDL.cpp
+++ ws_SDL/ws_SDL.cpp
@@ -10,8 +10,29 @@ int main(int argc, char* argv[])
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
         return 1;
 
-    //800X600, centre of screen
+    //Creates 800X600 window at the centre of the screen
     SDL_Window* window = SDL_CreateWindow("James Eyre: 26464488 | CGP2015M", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+    //Creates a renderer which draws to the window surface
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+
+    //Clears everything on the window -- paint over with last know colour
+    SDL_RenderClear(renderer);
+
+    //Creates a rectangle
+    SDL_Rect r;
+    r.x = 30;
+    r.y = 30;
+    r.w = 100;
+    r.h = 50;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawRect(renderer, &r);
+
+
+    //Tell SDL we have finished drawing, SDL will actually show our changes
+    SDL_RenderPresent(renderer);
 
     SDL_Event event;
     bool isFullscreen = false;
